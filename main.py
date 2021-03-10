80e8fa34832f9bc1e732909106ce1c56c9d585d2
Thomas mclean <tj.mclean93@gmail.com>
2021-03-10 10:24:17 -0500

    second commit just modules

---
+++ main.py
@@ -0,0 +1,19 @@
+import configparser
+import json
+import logging
+import mechanicalsoup
+import re
+import smtplib
+import sys
+from os.path import dirname, abspath
+
+path = dirname(abapath(_file_))
+recipientsPath = f'{path}/recipients.json '
+
+client = smtplib.SMTP('localhost')
+
+def send_email(user, pwd, recipients, subject, body):
+    gmail_user = gpustocker2021@gmail.com
+    gmail_pwd = BennyButcher2020
+    FROM = gpustocker
+    TO =
