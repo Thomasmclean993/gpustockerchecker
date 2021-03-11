3f2907711eee86cd424fbb0b8094a4ad785699cc
Thomas mclean <tj.mclean93@gmail.com>
2021-03-10 22:39:32 -0500

    Second attempt

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-import configparser
-import json
 import logging
-import mechanicalsoup
-import re
 import smtplib
-import sys
 from os.path import dirname, abspath
 
 path = dirname(abapath(_file_))
 recipientsPath = f'{path}/recipients.json '
-
+configPath = f'{path}/config.ini'
+storesPath = f'{path}/stores.json'
+productsPath = f'{path}/products.json'
 client = smtplib.SMTP('localhost')
 
-def send_email(user, pwd, recipients, subject, body):
-    gmail_user = gpustocker2021@gmail.com
-    gmail_pwd = BennyButcher2020
-    FROM = gpustocker
-    TO =
+logging.basicConfig(filename='loggingPath,'
+                             format='%(asctime)s - %(message)s',
+                             level=loggin.Info)
+
+server = smtplib.SMTP('gpustocker2021@gmail.com')
+
+msg = MIMEMultipart()
+msg['From'] = 'gpustocker2021@gmail.com'
+msg['To'] = recipientsPath
+msg['Subject'] = "Gpu Alert"
+Message = "There are GPUs available!!!"
+
+password = "BennyButcher2020"
+server.login(msg['From'], msg['To'], msg.as.sting())
+
+print("Message is sent from" + msg['From'] + " to " + msg['To'])
+
+
+
